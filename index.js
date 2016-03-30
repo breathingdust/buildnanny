f2b82b4c5b2db1fbd25c7bef7f69c68850dab2b8
Simon Davis <simon@breathingdust.com>
2016-03-29 22:13:52 -0700

    adding some of the teamcity interaction skeleton

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var CronJob = require('cron').CronJob;
 var config = require('./config');
+var request = require('request');
 
 function init(){
   for (var i = 0; i < config.cronjobs.length; i++) {
@@ -14,7 +15,26 @@ function init(){
 }
 
 function checkTeamcity(){
+  var teamcity_host = config.teamcity_host;
+  var session = getTeamcitySession(teamcity_host, teamcity_username, teamcity_password);
+
+  var buildConfigurations = [];
+
+  buildConfigurations.push(getConfigurationsForProjects(config.projects));
+  buildConfigurations.push(config.buildConfigurations);
+
+  var failingBuildConfigurations = getFailingBuilds(buildConfigurations);
+  
   return {"msg":"go look at team city"};
+}
+
+function getTeamcitySession(host, username, password){
+  request.get('http://some.server.com/').auth('username', 'password', false);
+
+}
+
+function constructTeamcityRequest(){
+  return request.get()
 }
 
 function performIntegrations(buildStatus){
